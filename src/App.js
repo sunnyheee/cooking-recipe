c05a3a086724bfe9ea20806a66a86d08efad6f35
sunnyheee <fool0116@naver.com>
2024-04-15 22:23:36 +0900

    fix: edit AppLayout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route } from 'react-router-dom';
-import Mainpage from './mainpage.jsx'
-import AppLayout from './layout/AppLayout.jsx'
-import SearchRecipe from './pages/SearchRecipe/SearchRecipe.jsx'
-import NotFoundPage from './pages/NotFound/NotFoundPage.jsx'
-import RecipeDetail from './pages/RecipeDetail/RecipeDetail.jsx'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
+import Mainpage from "./mainpage.jsx";
+import AppLayout from "./layout/AppLayout.jsx";
+import SearchRecipe from "./pages/SearchRecipe/SearchRecipe.jsx";
+import NotFoundPage from "./pages/NotFound/NotFoundPage.jsx";
+import RecipeDetail from "./pages/RecipeDetail/RecipeDetail.jsx";
 
 function App() {
-  return <>
-    <Routes>
-      <Route path="/" element={ <AppLayout/> } />
-        <Route index element={ <Mainpage/> } />
-        <Route path="search">
-          <Route index element={ <SearchRecipe/> } />
-          <Route path=":id" element = { <RecipeDetail/>} />
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Mainpage />} />
+          <Route path="search">
+            <Route index element={<SearchRecipe />} />
+            <Route path=":id" element={<RecipeDetail />} />
+          </Route>
         </Route>
-      <Route path="*" element={ <NotFoundPage/> } />
-    </Routes>
-  </>;
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
