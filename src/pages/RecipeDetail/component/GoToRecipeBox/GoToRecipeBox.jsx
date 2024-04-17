8640c52c0c82755fee004a3010a007bd8a76dbaa
Hojae Lee <rugbyhj99@naver.com>
2024-04-18 00:28:49 +0900

    ...

--- src/pages/RecipeDetail/component/GoToRecipeBox/GoToRecipeBox.jsx
+++ src/pages/RecipeDetail/component/GoToRecipeBox/GoToRecipeBox.jsx
@@ -6,12 +6,12 @@ const GoToRecipeBox = ({recipe}) => {
  
   return (
     <Container>
-            <div>
-                <Button variant="primary" size="lg" href={recipe.url}>
-                    View recipe
-                </Button>            
-            </div>
-            <div>좋아요</div>    
+        <div>
+            <Button variant="primary" size="lg" href={recipe.url}>
+                View recipe
+            </Button>            
+        </div>
+        <div>좋아요</div>    
              
     </Container>
   )
